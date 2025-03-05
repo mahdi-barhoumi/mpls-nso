a80e7bee76ca2cea137ac91aef6d53acb9ba3345
BadjoGit <mehdi192001@yahoo.fr>
2025-03-05 15:26:46 +0100

    Deleted loose file

--- src/restconf/restconf.py
+++ src/restconf/restconf.py
@@ -8,8 +8,8 @@ ip = "192.168.100.101"
 username = "mgmt"
 password = "mgmtapp"
 method = "GET"
-container = "Cisco-IOS-XE-native:native"
-leaf = "hostname"
+container = "Cisco-IOS-XE-device-hardware-oper:device-hardware-data/device-hardware"
+leaf = ""
 headers = {
     'accept': "application/yang-data+json",
     'content-type': "application/yang-data+json"
