18e45b25fc97be30a27d279c69619d116dce3916
Fyroo <chaouimoutaa@gmail.com>
2025-05-02 19:58:04 +0100

    Refactor AppMenu structure for improved organization and clarity

--- frontend/src/layout/AppMenu.vue
+++ frontend/src/layout/AppMenu.vue
@@ -1,110 +1,88 @@
 <script setup>
 import { ref } from 'vue'
-
 import AppMenuItem from './AppMenuItem.vue'
 
 const model = ref([
   {
-    label: 'Home',
-    items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' }],
-  },
-  {
-    label: 'Pages',
-    icon: 'pi pi-fw pi-briefcase',
-    to: '/pages',
+    label: 'Overview',
+    icon: 'pi pi-fw pi-th-large',
     items: [
       {
-        label: 'Customers',
-        icon: 'pi pi-fw pi-users',
-        to: '/customers',
+        label: 'Dashboard',
+        icon: 'pi pi-fw pi-home',
+        to: '/',
       },
-      {
-        label: 'Sites',
-        icon: 'pi pi-fw pi-building',
-        to: '/sites',
-      },
-      {
-        label: 'VPN',
-        icon: 'pi pi-fw pi-sitemap',
-        to: '/vpn',
-      },
-      // {
-      //   label: 'Auth',
-      //   icon: 'pi pi-fw pi-user',
-      //   items: [
-      //     {
-      //       label: 'Login',
-      //       icon: 'pi pi-fw pi-sign-in',
-      //       to: '/auth/login',
-      //     },
-      //     {
-      //       label: 'Error',
-      //       icon: 'pi pi-fw pi-times-circle',
-      //       to: '/auth/error',
-      //     },
-      //     {
-      //       label: 'Access Denied',
-      //       icon: 'pi pi-fw pi-lock',
-      //       to: '/auth/access',
-      //     },
-      //   ],
-      // },
-      // {
-      //   label: 'Not Found',
-      //   icon: 'pi pi-fw pi-exclamation-circle',
-      //   to: '/pages/notfound',
-      // },
-      // {
-      //   label: 'Empty',
-      //   icon: 'pi pi-fw pi-circle-off',
-      //   to: '/pages/empty',
-      // },
     ],
   },
-  // {
-  //   label: 'Hierarchy',
-  //   items: [
-  //     {
-  //       label: 'Submenu 1',
-  //       icon: 'pi pi-fw pi-bookmark',
-  //       items: [
-  //         {
-  //           label: 'Submenu 1.1',
-  //           icon: 'pi pi-fw pi-bookmark',
-  //           items: [
-  //             { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
-  //             { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
-  //             { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' },
-  //           ],
-  //         },
-  //         {
-  //           label: 'Submenu 1.2',
-  //           icon: 'pi pi-fw pi-bookmark',
-  //           items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }],
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       label: 'Submenu 2',
-  //       icon: 'pi pi-fw pi-bookmark',
-  //       items: [
-  //         {
-  //           label: 'Submenu 2.1',
-  //           icon: 'pi pi-fw pi-bookmark',
-  //           items: [
-  //             { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
-  //             { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' },
-  //           ],
-  //         },
-  //         {
-  //           label: 'Submenu 2.2',
-  //           icon: 'pi pi-fw pi-bookmark',
-  //           items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }],
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // },
+
+  {
+    label: 'Tools',
+    items: [
+      {
+        label: 'Monitoring',
+        icon: 'pi pi-fw pi-chart-bar',
+        to: '/monitoring',
+      },
+      {
+        label: 'Administration',
+        icon: 'pi pi-fw pi-briefcase',
+        items: [
+          {
+            label: 'Customers',
+            icon: 'pi pi-fw pi-users',
+            to: '/customers',
+          },
+          {
+            label: 'Sites',
+            icon: 'pi pi-fw pi-building',
+            to: '/sites',
+          },
+          {
+            label: 'VPN',
+            icon: 'pi pi-fw pi-sitemap',
+            to: '/vpn',
+          },
+          {
+            label: 'Device Inventory',
+            icon: 'pi pi-fw pi-server',
+            to: '/device-inventory',
+          },
+        ],
+      },
+      {
+        label: 'Settings',
+        icon: 'pi pi-fw pi-cog',
+        items: [
+          {
+            label: 'User Settings',
+            icon: 'pi pi-fw pi-user-edit',
+            to: '/settings/user',
+          },
+          {
+            label: 'Server Settings',
+            icon: 'pi pi-fw pi-sliders-h',
+            to: '/settings/server',
+          },
+        ],
+      },
+      {
+        label: 'Troubleshooting',
+        icon: 'pi pi-fw pi-wrench',
+        items: [
+          {
+            label: 'Logs',
+            icon: 'pi pi-fw pi-file',
+            to: '/troubleshooting/logs',
+          },
+          {
+            label: 'Guide',
+            icon: 'pi pi-fw pi-book',
+            to: '/troubleshooting/guide',
+          },
+        ],
+      },
+    ],
+  },
 ])
 </script>
 
