ddd578f03d5b2f84d6a9dd4c05c43174d46e6650
BadjoGit <mehdi192001@yahoo.fr>
2025-05-27 13:41:24 +0100

    fix: Correct customer reference

--- backend/core/modules/network_controller.py
+++ backend/core/modules/network_controller.py
@@ -628,7 +628,7 @@ class _NetworkController:
         all_possible_subnets = list(base_network.subnets(new_prefix=30))
         
         # Get all currently used link networks for the same customer
-        used_networks = Site.objects.filter(customer=self.customer).exclude(pk=self.pk).values_list('link_network', flat=True)
+        used_networks = Site.objects.filter(customer=site.customer).exclude(pk=self.pk).values_list('link_network', flat=True)
         used_networks = [ipaddress.ip_network(f"{ip}/30") for ip in used_networks if ip]
         
         # Find the first available subnet
